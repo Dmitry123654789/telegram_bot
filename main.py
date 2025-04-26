6df1b8a30662cc6110321a566724ca11c44e8165
Dmitry <pokemon2708@yandex.ru>
2025-04-26 21:49:58 +0300

    Таймер-бот 2.0

--- main.py
+++ main.py
@@ -1,56 +1,110 @@
-from telegram.ext import CommandHandler, Application
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Application,CommandHandler
 
-BOT_TOKEN = 'Token'
+import random
+import math
 
-TIMER = 0  # таймер на 0 секунд
-
-
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+TOKEN = '7306972602:AAFcRt44eELIQkbjybhrUk4CWuJgao4zZAo'
 
 
-async def set_timer(update, context):
-    """Добавляем задачу в очередь"""
-    global TIMER
-    try:
-        TIMER = int(context.args[0])
-        chat_id = update.effective_message.chat_id
-        job_removed = remove_job_if_exists(str(chat_id), context)
-        context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
-        text = f'Вернусь через {TIMER} с.!'
-        if job_removed:
-            text += ' Старая задача удалена.'
-        await update.effective_message.reply_text(text)
-    except ValueError:
-        await context.bot.send_message(context.job.chat_id, text=f'Некорректно передано время таймера')
-    except IndexError:
-        await context.bot.send_message(context.job.chat_id, text=f'Не передано время таймера')
+base_keyboard = [['/dice', '/timer']]
+dice_keyboard = [['/6', '/2x6', '/20', '/back']]
+timer_keyboard = [['/30s', '/1m', '/5m', '/back']]
+close_keyboard = [['/close']]
+
+base_markup = ReplyKeyboardMarkup(base_keyboard, one_time_keyboard=False)
+dice_markup = ReplyKeyboardMarkup(dice_keyboard, one_time_keyboard=False)
+timer_markup = ReplyKeyboardMarkup(timer_keyboard, one_time_keyboard=False)
+active_timer_markup = ReplyKeyboardMarkup(close_keyboard, one_time_keyboard=False)
 
 
-async def task(context):
-    """Выводит сообщение"""
-    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {TIMER}c. прошли!')
+async def start(update, context):
+    await update.message.reply_text('Привет! Я бот-гадалка!')
+    await update.message.reply_text("/dice: кинуть кубики, /timer: засечь время", reply_markup=base_markup)
 
 
-async def unset(update, context):
-    """Удаляет задачу, если пользователь передумал"""
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
-    await update.message.reply_text(text)
+async def dices(update, context):
+    await update.message.reply_text("кинуть кубики: 6 граней, 2 по 6, 20 или вернуться назад", reply_markup=dice_markup)
+
+
+async def dice6(update, context):
+    number = math.trunc(random.random() * 6) + 1
+    await update.message.reply_text("{0}".format(number))
+
+
+async def dice2x6(update, context):
+    number1 = math.trunc(random.random() * 6) + 1
+    number2 = math.trunc(random.random() * 6) + 1
+    await update.message.reply_text("{0} {1}".format(number1, number2))
+
+
+async def dice20(update, context):
+    number = math.trunc(random.random() * 20) + 1
+    await update.message.reply_text("{0}".format(number))
+
+
+# Управление таймерами.
+
+async def timers(update, context):
+    await update.message.reply_text("засечь: 30 сек., 1 мин., 5 мин.  или вернуться назад", reply_markup=timer_markup)
+
+
+async def set_timer(update, context, delay):
+    job = context.job_queue.run_once(finish_timer, delay, chat_id=update.message.chat_id, data=delay)
+
+    context.chat_data['job'] = job
+    await update.message.reply_text(f'Установлен таймер на {delay} секунд', reply_markup=active_timer_markup)
+
+
+async def finish_timer(context):
+    job = context.job
+    await context.bot.send_message(job.chat_id, text='Время истекло.', reply_markup=timer_markup)
+
+
+async def reset_timer(update, context):
+    if 'job' in context.chat_data:
+        context.chat_data['job'].schedule_removal()
+        del context.chat_data['job']
+
+    await update.message.reply_text('Таймер сброшен.', reply_markup=timer_markup)
+
+
+# Таймеры
+
+async def timer30s(update, context):
+    await set_timer(update, context, 30)
+
+
+async def timer1m(update, context):
+    await set_timer(update, context, 60)
+
+
+async def timer5m(update, context):
+    await set_timer(update, context, 300)
 
 
 def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("set_timer", set_timer))
-    application.add_handler(CommandHandler("unset_timer", unset))
+    application = Application.builder().token(TOKEN).build()
+
+    # Старт
+    application.add_handler(CommandHandler("start", start))
+
+    # Переключение режимов
+    application.add_handler(CommandHandler("dice", dices))
+    application.add_handler(CommandHandler("timer", timers))
+    application.add_handler(CommandHandler("back", start))
+
+    # Кубики
+    application.add_handler(CommandHandler("6", dice6))
+    application.add_handler(CommandHandler("2x6", dice2x6))
+    application.add_handler(CommandHandler("20", dice20))
+
+    # Таймеры
+    application.add_handler(CommandHandler("30s", timer30s))
+    application.add_handler(CommandHandler("1m", timer1m))
+    application.add_handler(CommandHandler("5m", timer5m))
+    application.add_handler(CommandHandler("close", reset_timer))
+
     application.run_polling()
 
 
