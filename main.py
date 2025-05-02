4fd8b2bb62a100e7ae6b475beaa2a838cffeb8cb
Dmitry <pokemon2708@yandex.ru>
2025-05-02 13:54:06 +0300

    Бот «Тестирующая система»

--- main.py
+++ main.py
@@ -1,55 +1,66 @@
+from itertools import count
+from json import loads
+from random import choice
 from telegram.ext import Application, CommandHandler, filters, MessageHandler
 
 from config import BOT_TOKEN
 
-poem = '''Белеет парус одинокой
-В тумане моря голубом!..
-Что ищет он в стране далекой?
-Что кинул он в краю родном?..
-Играют волны — ветер свищет,
-И мачта гнется и скрыпит...
-Увы! он счастия не ищет
-И не от счастия бежит!
-Под ним струя светлей лазури,
-Над ним луч солнца золотой…
-А он, мятежный, просит бури,
-Как будто в бурях есть покой!'''.split('\n')
+QUESTIONS = {}
+GAME = False
 
-COUNT = 0
+user_questions = {}
+last_questions = ''
 
 
 async def start(update, context):
-    global COUNT
-    COUNT = 1
-    await update.message.reply_text(poem[0])
+    if QUESTIONS:
+        await update.message.reply_text('''Хотите ли вы пройти опрос?\n/yes Да\n/no нет''')
+
+
+async def yes(update, context):
+    global GAME, user_questions, last_questions
+    GAME = True
+    res = choice(list(list(QUESTIONS.keys())))
+    user_questions = {res: ''}
+    last_questions = res
+    await update.message.reply_text("Первый вопрос")
+    await update.message.reply_text(res)
+
+
+async def no(update, context):
+    await update.message.reply_text("Ну и ладно")
 
 
 async def stop(update, context):
-    global COUNT
+    global GAME
+    GAME = False
     await update.message.reply_text("Игра прервана")
-    await update.message.reply_text("Чтобы начать с начала введите /start")
-    COUNT = len(poem)
 
 
-async def suphler(update, context):
-    await update.message.reply_text(poem[COUNT])
+async def load_question(update, context):
+    global last_questions, user_questions, GAME, QUESTIONS
+    if not GAME:
+        try:
+            for res in loads(update.message.text)['test']:
+                QUESTIONS[res['question']] = res['response']
+            return await update.message.reply_text("Вопросы записаны")
+        except Exception as e:
+            return await update.message.reply_text(f"Вопросы не записаны\nКод ошибки {str(e)}")
 
+    user_questions[last_questions] = update.message.text.strip()
+    if len(user_questions) == len(list(QUESTIONS.keys())) or len(user_questions) == 10:
+        GAME = False
+        count_true_answer = len([ansew for key, ansew in user_questions.items() if ansew == QUESTIONS[key]])
+        await update.message.reply_text(
+            f"Всего вопросов в тесте было {len(user_questions)}\nПравильный ответов {count_true_answer}")
+        return await update.message.reply_text(f"Хотите пройти тест снова?\n/yes Да\n/no нет")
 
-async def print_poem(update, context):
-    global COUNT
-    try:
-        if update.message.text == poem[COUNT]:
-            COUNT += 2
-            if COUNT <= len(poem):
-                await update.message.reply_text(poem[COUNT - 1])
-            if COUNT >= len(poem):
-                await update.message.reply_text('Я рад, что у нас получилось все рассказать\n'
-                                                'Что бы начать с начала введите /start')
-        else:
-            await update.message.reply_text('Нет, не так')
-            await suphler(update, context)
-    except IndexError:
-        await update.message.reply_text('Нет, не так')
+    res = choice(list(QUESTIONS.keys()))
+    while res in user_questions.keys():
+        res = choice(list(QUESTIONS.keys()))
+    user_questions[res] = ''
+    last_questions = res
+    await update.message.reply_text(res)
 
 
 def main():
@@ -57,8 +68,9 @@ def main():
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("stop", stop))
-    application.add_handler(CommandHandler("suphler", suphler))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, print_poem))
+    application.add_handler(CommandHandler("yes", yes))
+    application.add_handler(CommandHandler("no", no))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, load_question))
 
     application.run_polling()
 
